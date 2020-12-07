ae74c0e2485fc0258314934c473224759e38908e
kensleDev <kensledev@gmail.com>
2020-12-07 22:10:41 +0000

    :hammer: added awesome config

---
+++ awesome/.config/awesome/rc.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                   Base setup                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+
+-- Configuration file selection
+-----------------------------------------------------------------------------------------------------------------------
+-- local rc = "colorless.rc-colorless"
+
+--local rc = "color.red.rc-red"
+--local rc = "color.blue.rc-blue"
+-- local rc = "color.orange.rc-orange"
+--local rc = "color.green.rc-green"
+
+-- local rc = "shade.ruby.rc-ruby"
+-- local rc = "shade.steel.rc-steel"
+
+local rc = "kensleDev.rc-kensleDev"
+
+require(rc)
